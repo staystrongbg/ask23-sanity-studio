cb496c625c75729e5fad95b90aedeaa9378a29c9
staystrongbg <staystrongbg@gmail.com>
2023-09-26 12:09:26 +0200

    global css head

--- src/components/Pages/Kategorije.tsx
+++ src/components/Pages/Kategorije.tsx
@@ -2,7 +2,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { CategoryCard, CategoryTitle, CategoryImgContainer } from '../utils';
-
 const Kategorije = () => {
   const categoryData = [
     {
